2688502dc1ddbc7ba141593a61b8db9b21954ac6
unknown <jutionck@gmail.com>
2022-03-17 12:07:50 +0700

    Able to delete data

--- layout/body/student/index.php
+++ layout/body/student/index.php
@@ -39,26 +39,3 @@
         </div>
     </div>
 </div>
-<script>
-    $(document).ready(function() {
-        $(document).on('click', '.delete-student', function() {
-            let id = $(this).data("id");
-            if (confirm("Are you sure to delete this record ?")) {
-                $.ajax({
-                    url: "layout/body/student/delete.php",
-                    type: "GET",
-                    data: {
-                        id: id
-                    },
-                    error: function() {
-                        alert("Something is wrong, couldn't delete record");
-                    },
-                    success: function(data) {
-                        alert("Record delete successfully.")
-                        window.location.href = "index.php?page=students"
-                    }
-                })
-            }
-        })
-    })
-</script>
--- layout/partial/footer.php
+++ layout/partial/footer.php
@@ -1,4 +1,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="<?= baseURL("") ?>/assets/js/script.js"></script>
 </body>
 
 </html>
